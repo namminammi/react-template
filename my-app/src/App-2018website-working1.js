266c3b9b8690d8da8bb4eaee0c41ab97cdc65a4e
“namminammi” <nammionline@gmail.com>
2018-01-08 21:29:33 -0500

    sortable website

--- my-app/src/App-2018website-working1.js
+++ my-app/src/App-2018website-working1.js
@@ -1,39 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-// import Toggle from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-import FilterableProductTable from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// ReactDOM.render((
-//   <Toggle />
-// ), document.getElementById('root'));
-
-// const PRODUCTS = [
-//   {category: 'SportingGoods', price: '$49.99', stocked: true, name: 'Football'},
-//   {category: 'SportingGoods', price: '$9.99', stocked: true, name: 'Baseball'},
-//   {category: 'SportingGoods', price: '$29.99', stocked: false, name: 'Basketball'},
-//   {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-//   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-//   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-// ];
+import { render } from 'react-dom';
 
 
-const PRODUCTS = 
-      [
+var projects = {
+      
+    list : {
+      links: [
       {
                 "link_name": "Outerplaces.com Events page (Joomla) ",
-                "category" : "TeamWorks",
+                "category" : "Team Works",
                 "website": "http://www.outerplaces.com/events-directory",
                 "desc": "A page for science and science fiction related events, filtering out past events and showing relevant items according to the theme."
             }, 
             {
                 "link_name": "LightTV.com (Wordpress)",
-                "category" : "TeamWorks",
+                "category" : "Team Works",
                 "website": "https://www.lighttv.com/",
                 "desc": "A streaming website with family-friendly contents by MGM, integrating video player by Akamai and scheduling by Gracenote."
       
@@ -41,32 +22,32 @@ const PRODUCTS =
       
             {
                 "link_name": "Novo Surgical (Magento) ",
-                "category" : "TeamWorks",
+                "category" : "Team Works",
                 "website": "https://novosurgical.com/",
                 "desc": "Magento-based e-commerce website for an internaltional surgical instrument company."
             }, 
             {
                 "link_name": "Movie Search App (React)",
-                "category" : "PersonalProjects",
+                "category" : "Personal Projects",
                 "website": "https://showmemoviesnow.herokuapp.com/",
                 "desc": "Movie search app using The Movie Database API."
             },
       
             {
                 "link_name": "Scandinavian Countries (AngularJS)",
-                "category" : "PersonalProjects",
+                "category" : "Personal Projects",
                 "website": "http://namminammi.github.io/Scandinavian/Scandinavian_AngularJS_withQuizz.html",
                 "desc": "A Single Page App using AngularJS, showing maps and a list of checkbox items for each country along with trivia quizz."
             }, 
             {
                 "link_name": "Waterproof Blue Doors",
-                "category" : "PersonalProjects",
+                "category" : "Personal Projects",
                 "website": "http://namminammi.github.io/LegoOnePage/indexLego2.html",
                 "desc": "A parallax page anchored to show each character of the story."
             }, 
             {
                 "link_name": "2017 American Eclipse App (Ajax calls)",
-                "category" : "PersonalProjects",
+                "category" : "Personal Projects",
                 "website": "http://namminammi.github.io/Eclipse2017/eclipse1.html",
                 "desc": "A page to calculate the time and distance of the 2017 Great American Eclipse based on zipcode, using zippopotam, usno.navy and google map API."
       
@@ -74,49 +55,49 @@ const PRODUCTS =
       
             {
                 "link_name": "Dessertful Christmas Card",
-                "category" : "PersonalProjects",
+                "category" : "Personal Projects",
                 "website": "http://namminammi.github.io/Xmas_card/xmas_card.html",
                 "desc": "An interactive Christmas card made with jQuery."
             }, 
             {
                 "link_name": "Toggle Articles",
-                "category" : "PersonalProjects",
+                "category" : "Personal Projects",
                 "website": "http://namminammi.github.io/ToggleArticle/ToggleArticle.html",
                 "desc": "A page to show the same story in different languages when the user clicks the corresponding items."
             },
             {
                 "link_name": "Sortable JSON Table",
-                "category" : "FrontEndTests",
+                "category" : "Front End Tests",
                 "website": "http://namminammi.github.io/JSON_sortable_table/task.html",
                 "desc": "A table populated with JSON data, sorted by clicking the grey header cell."
             },
             {
                 "link_name": "News Module",
-                "category" : "FrontEndTests",
+                "category" : "Front End Tests",
                 "website": "http://namminammi.github.io/News_module/news_module_index.html",
                 "desc": "A news module that loads nex and previous page, changing the direction of arrows accordingly."
             },
             {
                 "link_name": "Page Design Translation (SASS)",
-                "category" : "FrontEndTests",
+                "category" : "Front End Tests",
                 "website": "http://namminammi.github.io/FE_Challenge_scss/FE_Challenge_scss.html",
                 "desc": "PSD page translated into code using SASS."
             },
             {
                 "link_name": "Tabs Showing Content On Hover",
-                "category" : "FrontEndTests",
+                "category" : "Front End Tests",
                 "website": "http://namminammi.github.io/NamChoi_FE_Task_tab/FE_task_tab2.html",
                 "desc": "Tabs showing content on hover using CSS only"
             },
             {
                 "link_name": "Custom Numeric Slider",
-                "category" : "FrontEndTests",
+                "category" : "Front End Tests",
                 "website": "http://namminammi.github.io/CustomSlider/custom_slider.html",
                 "desc": "A Custom slider that shows the number in range when 1) blue ball is dragged or 2) - or + sign is clicked."
             },
             {
                 "link_name": "Parallax page translation",
-                "category" : "FrontEndTests",
+                "category" : "Front End Tests",
                 "website": "http://namminammi.github.io/Demo1/Demo1.html",
                 "desc": "Parallax page translated from PSD file."
             },
@@ -127,11 +108,76 @@ const PRODUCTS =
                 "desc": "Recurring interview questions in job interviews."
             }
             ]
+          }
+      };
 
 
-ReactDOM.render((
-  <FilterableProductTable products={PRODUCTS}/>
-), document.getElementById('root'));
+class links extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShow: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+       isShow: !prevState.isShow 
+    }))
+  }
+
+  contentClass(isShow) {
+    if (isShow) {
+      return "content";
+    }
+    return "content invisible";
+  }
+
+  render() {
+    return (
+      <div className='control' onClick={this.handleClick}>
+        <h4>{this.props.desc}</h4>
+        <div className={this.contentClass(this.state.isShow)}>{this.props.link_name}</div>
+      </div>
+    )
+  }
+}
+
+class Toggle extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = { 
+       isShow: false,
+       id: -1, 
+     };
+   }
+
+   handleClick = (id) => {
+    this.setState({ 
+      isShow: !this.state.isShow,
+      id: id
+    });
+   }
+
+   render() {
+     const { links } = projects.list;
+     const { isShow, id } = this.state;
+     return (
+     
+       <div className="control container">
+          {links.map((loc, index) => (
+            <div key={index} onClick={() => { this.handleClick(index) }}>
+              <h4>{loc.link_name}</h4>
+              {(isShow && (id === index)) && <p>{loc.desc} {"\n"} {"\n"} <a href={loc.website} target='_blank'>links</a></p>}
+            </div>
+           ))}
+       </div>
 
 
-registerServiceWorker();
+      
+      );
+   }
+}
+
+
+export default Toggle;
+
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,166 +2,136 @@ import React from 'react';
 import { render } from 'react-dom';
 
 
-var projects = {
-      
-    list : {
-      links: [
-      {
-                link_name: "Outerplaces.com Events page (Joomla) ",
-                "website": "http://www.outerplaces.com/events-directory",
-                "desc": "A page for science and science fiction related events, filtering out past events and showing relevant items according to the theme."
-            }, 
-            {
-                link_name: "LightTV.com (Wordpress)",
-                "website": "https://www.lighttv.com/",
-                "desc": "A streaming website with family-friendly contents by MGM, integrating video player by Akamai and scheduling by Gracenote."
-      
-            },
-      
-            {
-                "link_name": "Novo Surgical (Magento) ",
-                "website": "https://novosurgical.com/",
-                "desc": "Magento-based e-commerce website for an internaltional surgical instrument company."
-            }, 
-            {
-                "link_name": "Movie Search App (React)",
-                "website": "https://showmemoviesnow.herokuapp.com/",
-                "desc": "Movie search app using The Movie Database API."
-            },
-      
-            {
-                "link_name": "Scandinavian Countries (AngularJS)",
-                "website": "http://namminammi.github.io/Scandinavian/Scandinavian_AngularJS_withQuizz.html",
-                "desc": "A Single Page App using AngularJS, showing maps and a list of checkbox items for each country along with trivia quizz."
-            }, 
-            {
-                "link_name": "Waterproof Blue Doors",
-                "website": "http://namminammi.github.io/LegoOnePage/indexLego2.html",
-                "desc": "A parallax page anchored to show each character of the story."
-            }, 
-            {
-                "link_name": "2017 American Eclipse App (Ajax calls)",
-                "website": "http://namminammi.github.io/Eclipse2017/eclipse1.html",
-                "desc": "A page to calculate the time and distance of the 2017 Great American Eclipse based on zipcode, using zippopotam, usno.navy and google map API."
-      
-            },
-      
-            {
-                "link_name": "Dessertful Christmas Card",
-                "website": "http://namminammi.github.io/Xmas_card/xmas_card.html",
-                "desc": "An interactive Christmas card made with jQuery."
-            }, 
-            {
-                "link_name": "Toggle Articles",
-                "website": "http://namminammi.github.io/ToggleArticle/ToggleArticle.html",
-                "desc": "A page to show the same story in different languages when the user clicks the corresponding items."
-            },
-            {
-                "link_name": "Sortable JSON Table",
-                "website": "http://namminammi.github.io/JSON_sortable_table/task.html",
-                "desc": "A table populated with JSON data, sorted by clicking the grey header cell."
-            },
-            {
-                "link_name": "News Module",
-                "website": "http://namminammi.github.io/News_module/news_module_index.html",
-                "desc": "A news module that loads nex and previous page, changing the direction of arrows accordingly."
-            },
-            {
-                "link_name": "Page Design Translation (SASS)",
-                "website": "http://namminammi.github.io/FE_Challenge_scss/FE_Challenge_scss.html",
-                "desc": "PSD page translated into code using SASS."
-            },
-            {
-                "link_name": "Tabs Showing Content On Hover",
-                "website": "http://namminammi.github.io/NamChoi_FE_Task_tab/FE_task_tab2.html",
-                "desc": "Tabs showing content on hover using CSS only"
-            },
-            {
-                "link_name": "Custom Numeric Slider",
-                "website": "http://namminammi.github.io/CustomSlider/custom_slider.html",
-                "desc": "A Custom slider that shows the number in range when 1) blue ball is dragged or 2) - or + sign is clicked."
-            },
-            {
-                "link_name": "Parallax page translation",
-                "website": "http://namminammi.github.io/Demo1/Demo1.html",
-                "desc": "Parallax page translated from PSD file."
-            },
-            {
-                "link_name": "Frequently Asked Front End Interview Questions",
-                "website": "https://github.com/namminammi/frequently-asked-front-end-interview-questions/blob/master/README.md",
-                "desc": "Recurring interview questions in job interviews."
-            }
-            ]
-          }
-      };
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const link_name = product.link_name;
+    const website = product.website;
+    const desc = product.desc;
+
+    return (
+      <div  className='control'>
+        <h4>{link_name}</h4>
+        <div className="description">
+        <p>{desc}</p>
+        <a href={website} target="_blank">link</a></div>
+      </div>
+    );
+  }
+}
 
 
-class links extends React.Component {
+class ProductTable extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      if (product.category.indexOf(filterText) === -1) {
+        return;
+      }
+
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.link_name}
+        />
+      );
+      lastCategory = product.category;
+    });
+
+    return (
+      <div className="container">
+        {rows}
+      </div>       
+    );
+  }
+}
+
+class SearchButtons extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isShow: false };
-    this.handleClick = this.handleClick.bind(this);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+  }
+  
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+ 
+  render() {
+    return (
+     
+      <form>
+        <input type="button" 
+          placeholder="TeamWorks"
+          value="TeamWorks"
+          onClick={this.handleFilterTextChange}
+          />
+       
+        <input type="button" 
+          placeholder="PersonalProjects"
+          value="PersonalProjects"
+          onClick={this.handleFilterTextChange}
+          />
+       
+          <input type="button" 
+          placeholder="FrontEndTests"
+          value="FrontEndTests"
+          onClick={this.handleFilterTextChange}
+          />
+       
+          <input type="button" 
+          placeholder="Etc"
+          value="Etc"
+          onClick={this.handleFilterTextChange}
+          />
+       
+        <input type="button" 
+          placeholder="See All"
+          value=""
+          onClick={this.handleFilterTextChange}
+          /> <label>See All</label>
+       
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: ''
+    };
+    
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
   }
 
-  handleClick() {
-    this.setState(prevState => ({
-       isShow: !prevState.isShow 
-    }))
-  }
-
-  contentClass(isShow) {
-    if (isShow) {
-      return "content";
-    }
-    return "content invisible";
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
   }
 
   render() {
     return (
-      <div className='control' onClick={this.handleClick}>
-        <h4>{this.props.desc}</h4>
-        <div className={this.contentClass(this.state.isShow)}>{this.props.link_name}</div>
+      <div>
+        <SearchButtons
+          filterText={this.state.filterText}
+          onFilterTextChange={this.handleFilterTextChange}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          />
       </div>
-    )
+    );
   }
 }
 
-class Toggle extends React.Component {
-   constructor(props) {
-     super(props);
-     this.state = { 
-       isShow: false,
-       id: -1, 
-     };
-   }
 
-   handleClick = (id) => {
-    this.setState({ 
-      isShow: !this.state.isShow,
-      id: id
-    });
-   }
-
-   render() {
-     const { links } = projects.list;
-     const { isShow, id } = this.state;
-     return (
-     
-       <div className="control container">
-          {links.map((loc, index) => (
-            <div key={index} onClick={() => { this.handleClick(index) }}>
-              <h4>{loc.link_name}</h4>
-              {(isShow && (id === index)) && <p>{loc.desc} {"\n"} {"\n"} <a href={loc.website} target='_blank'>links</a></p>}
-            </div>
-           ))}
-       </div>
+export default FilterableProductTable;
 
 
-      
-      );
-   }
-}
-
-
-export default Toggle;
 
