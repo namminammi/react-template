ca6dbc28d899a06d801befa6331fb734d40f3e11
“namminammi” <nammi@nyu.edu>
2020-05-05 16:14:44 -0400

    hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,49 +1,44 @@
 import React from 'react';
 
-class ProductRow extends React.Component {
-  render() {
-    const product = this.props.product;
-    const link_name = product.link_name;
-    const website = product.website;
-    const desc = product.desc;
+const ProductRow = ({ ...props }) => {
+  const product = props.product;
+  const link_name = product.link_name;
+  const website = product.website;
+  const desc = product.desc;
 
-    return (
-      <div  className='control'>
-        <h4>{link_name}</h4>
-        <div className="description">
+  return (
+    <div className='control'>
+      <h4>{link_name}</h4>
+      <div className="description">
         <p>{desc}</p>
         <a href={website} target="_blank" rel="noopener noreferrer">link</a></div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
+const ProductTable = ({ ...props }) => {
+  
+  const filterText = props.filterText;
+  const rows = [];
 
-class ProductTable extends React.Component {
-  render() {
-    const filterText = this.props.filterText;
-    const rows = [];
+  props.products.forEach((product) => {
+    if (product.category.indexOf(filterText) === -1) {
+      return;
+    }
 
-    this.props.products.forEach((product) => {
-      if (product.category.indexOf(filterText) === -1) {
-        return;
-      }
-
-      rows.push(
-        <ProductRow
-          product={product}
-          key={product.link_name}
-        />
-      );
-      
-    });
-
-    return (
-      <div className="container">
-        {rows}
-      </div>       
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.link_name}
+      />
     );
-  }
+  });
+
+  return (
+    <div className="container">
+      {rows}
+    </div>
+  );
 }
 
 class SearchButtons extends React.Component {
@@ -51,45 +46,45 @@ class SearchButtons extends React.Component {
     super(props);
     this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
   }
-  
+
   handleFilterTextChange(e) {
     this.props.onFilterTextChange(e.target.value);
   }
- 
+
   render() {
     return (
-     
+
       <form>
-        <input type="button" 
+        <input type="button"
           placeholder="TeamWorks"
           value="TeamWorks"
           onClick={this.handleFilterTextChange}
-          />
-       
-        <input type="button" 
+        />
+
+        <input type="button"
           placeholder="PersonalProjects"
           value="PersonalProjects"
           onClick={this.handleFilterTextChange}
-          />
-       
-          <input type="button" 
+        />
+
+        <input type="button"
           placeholder="FrontEndTests"
           value="FrontEndTests"
           onClick={this.handleFilterTextChange}
-          />
-       
-          <input type="button" 
+        />
+
+        <input type="button"
           placeholder="Etc"
           value="Etc"
           onClick={this.handleFilterTextChange}
-          />
-       
-        <input type="button" 
+        />
+
+        <input type="button"
           placeholder="See All"
           value=""
           onClick={this.handleFilterTextChange}
-          /> <label>See All</label>
-       
+        /> <label>See All</label>
+
       </form>
     );
   }
@@ -101,7 +96,7 @@ class FilterableProductTable extends React.Component {
     this.state = {
       filterText: ''
     };
-    
+
     this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
   }
 
@@ -121,7 +116,7 @@ class FilterableProductTable extends React.Component {
         <ProductTable
           products={this.props.products}
           filterText={this.state.filterText}
-          />
+        />
       </div>
     );
   }
